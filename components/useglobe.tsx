5ed1fe83df0c4ed5415cd56bc7993a116bf4b323
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-05-28 23:49:18 +0530

    fixes

--- components/useglobe.tsx
+++ components/useglobe.tsx
@@ -413,7 +413,7 @@ export function GlobeDemo() {
         >
   <h2 className="text-center text-xl md:text-4xl font-bold text-black dark:text-white">Join Our network!</h2>
   <p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
-  Imagine run models on multiple inferences for free !you dont understand right!? yes even i don't! </p>
+  Ready to elevate your social media experience? Say hello to ChirpUp 🐦!</p>
   <h2 className="text-center text-xl md:text-4xl font-bold text-black dark:text-white">Transform Your Social Interactions!</h2>
         </motion.div>
         <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
