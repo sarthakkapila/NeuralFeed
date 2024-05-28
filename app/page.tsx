75499cd0463b6341aa7a6c12797365c21f351eff
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-05-28 23:49:03 +0530

    frontend

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,17 @@ import { Cards } from "@/components/card";
 import { Waitlist } from "@/components/waitlist";
 import { HoverBorderGradientDemo } from "@/components/hoverbutton";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+    <Image
+      src="/public/dove.png"
+      width={25}
+      height={25}
+      alt="Bird"
+    />
       <div className=" table-auto flex justify-end p-5 border border-blue-950 bg-black">
         <button className="inline-flex h-11 w-36 animate-shimmer items-center justify-center rounded-2xl border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
           Log In
