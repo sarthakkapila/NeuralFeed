45ffd80480e30bdcca0a795367faa4289fc9dbe4
Sarthak Kapila <sarthakkapila27x@gmail.com>
2024-05-28 23:16:59 +0530

    fixes moving card contents

--- components/movingcard.tsx
+++ components/movingcard.tsx
@@ -18,31 +18,26 @@ export function InfiniteMovingCardsDemo() {
 const testimonials = [
   {
     quote:
-      "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-    name: "Charles Dickens",
-    title: "A Tale of Two Cities",
+      "“Your time is limited, so don't waste it living someone else's life.” ...",
+    name: "Steve Jobs",
   },
   {
     quote:
       "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
     name: "William Shakespeare",
-    title: "Hamlet",
   },
   {
-    quote: "All that we see or seem is but a dream within a dream.",
-    name: "Edgar Allan Poe",
-    title: "A Dream Within a Dream",
-  },
-  {
-    quote:
-      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-    name: "Jane Austen",
-    title: "Pride and Prejudice",
+    quote: "I don't mean to be presumptuous, but I liken myself to the robber barons.",
+    name: "Martin Shkreli",
   },
   {
     quote:
-      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-    name: "Herman Melville",
-    title: "Moby-Dick",
+      "You only lose what you cling to.",
+    name: "Gautam Budhha",
+  },
+  {
+    quote:
+      "“I think it is possible for ordinary people to choose to be extraordinary.” - ...",
+    name: "Elon Musk",
   },
 ];
